67d365502459fa06204fc02e5802acfd33f498f9
elinsecl <lynxjosue@gmail.com>
2025-06-22 15:50:27 -0600

    Aplique lo que dice Emma

--- controlador/PlatilloApiController.php
+++ controlador/PlatilloApiController.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type*");.
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+
 require_once __DIR__.'/../accesoDatos/PlatilloDAO.php'; 
 require_once __DIR__.'/../modelo/Platillo.php'; 
 
