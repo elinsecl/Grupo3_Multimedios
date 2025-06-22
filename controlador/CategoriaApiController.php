d67c4b5fef0a372fac0277d78124bd2402692005
elinsecl <lynxjosue@gmail.com>
2025-06-22 17:01:37 -0600

    cambios

--- controlador/CategoriaApiController.php
+++ controlador/CategoriaApiController.php
@@ -1,9 +1,9 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type*");.
+header("Access-Control-Allow-Origin: http://localhost:5174");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 
 require_once __DIR__.'/../accesoDatos/CategoriaDAO.php'; 
 require_once __DIR__.'/../modelo/Categoria.php'; 
