b43c4ba04033f59537262307aa8e9b5a024d5a25
elinsecl <lynxjosue@gmail.com>
2025-06-22 20:32:14 -0600

    xd

--- controlador/PlatilloApiController.php
+++ controlador/PlatilloApiController.php
@@ -1,10 +1,9 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
-
 
 require_once __DIR__.'/../accesoDatos/PlatilloDAO.php'; 
 require_once __DIR__.'/../modelo/Platillo.php'; 
